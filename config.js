b12a3d3ac420bdabeb9d16ca15d63b7d5428e306
Eoic <strazdas.karolis@outlook.com>
2019-09-11 20:19:48 +0300

    Add build pipeline

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
  * Localhost config
  */
 module.exports = {
-    mongoURI: 'mongodb://eoic:mongouser@localhost:27017/web-bots',
+    mongoURI: 'mongodb://karolis:mongouser@localhost:27017/web-bots',
     sessionKey: 'a_Z6deADFf8F+6e8f-cs',
     cookieAge: 86400000,
     devPort: 3000,
---
+++ src/server.ts
@@ -0,0 +1,5 @@
+let a = 10;
+a = 50;
+console.log(a);
+
+export {};
